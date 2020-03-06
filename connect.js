cabe3ac0158b9d46057630d13ab4de0536929cad
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-03-06 14:31:42 -0600

    create bid, and update sold.

--- connect.js
+++ connect.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -7,7 +8,7 @@ const connection = mysql.createConnection({
 
     user: "root",
 
-    password: "password",
+    password: "Sinceweonajet1199.",
 
     database: "great_bay"
 });
@@ -15,6 +16,57 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if(err) throw err;
     console.log("connected as id: " + connection.threadId + "\n");
-    //createBid();
+    createBid("cup", 5);
+    createBid("phone", 25);
 });
 
+const createBid = (item, price) => {
+    connection.query("INSERT INTO items SET ?",
+        {
+            item: item,
+            current_bid: price 
+        },
+        function(err, res) {
+            if (err) throw err;
+            timeOut(res.insertId);
+            console.log(res.affectedRows + " " + item + " auction inserted!\n");
+            
+        }
+    )
+}
+
+function selectAll() {
+    connection.query(`SELECT * FROM items`, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    });
+}
+
+const updateSold = (id) => {
+    connection.query("UPDATE items SET ? WHERE ?",
+    [
+        { sold: true},
+        { id: id}
+    ],
+    (err, res) => {
+        if(err) throw err;
+        selectAll();
+    })
+}
+
+const timeOut = (itemId) => {
+
+    let timer = 0;
+    const timeRun = setInterval( function() {
+        timer++;
+        if (timer > 45) {
+            clearInterval(timeRun);
+            updateSold(itemId)
+            selectAll();
+        }
+    }, 1000);
+}
+
+const slectId = (item) => {
+
+}
